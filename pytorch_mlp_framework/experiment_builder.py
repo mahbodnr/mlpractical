20a0a6b01fa06511894011dbfea511a75a783274
Mahbod <mahbodnouri@gmail.com>
2021-12-07 17:38:51 +0000

    minor change

--- pytorch_mlp_framework/experiment_builder.py
+++ pytorch_mlp_framework/experiment_builder.py
@@ -169,7 +169,7 @@ class ExperimentBuilder(nn.Module):
         plt.plot(all_grads, alpha=0.3, color="b")
         plt.hlines(0, 0, len(all_grads) + 1, linewidth=1, color="k")
         plt.xticks(
-            range(0, len(all_grads), 1), layers, rotation="vertical", fontsize="small"
+            range(0, len(all_grads), 1), layers, rotation="vertical", fontsize="x-small"
         )
         plt.xlim(xmin=0, xmax=len(all_grads))
         plt.xlabel("Layers")
